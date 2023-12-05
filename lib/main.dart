320c3330b7cdc5c92445038579ecd3aaa1764c23
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2023-12-05 17:47:01 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Container(
+        child: const Center(child: Text('Flutterando')),
+      ),
+    );
+  }
+}
